a2bf0d0287aaf25c97fd149282eedf1c6b3ba19e
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2014-05-12 11:28:07 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HydraFultonBag::Application.config.secret_key_base = 'e1858662098e7b2c2f22622360eab5e4fdb1b1513c46f632845a8daaae1bbb1ace4afa262e20ae4e626e57a494557c730318b8b54197c3704f0a29981e9a0ed3'
