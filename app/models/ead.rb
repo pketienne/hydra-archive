f0d107a98423e04215d69d524e6eacb0729df4c7
Patrick K. Etienne <github@informatician.me>
2014-05-12 18:16:58 -0400

    Need to remember to make more atomized commits...

--- app/models/ead.rb
+++ app/models/ead.rb
@@ -1,6 +1,8 @@
 class Ead < ActiveFedora::Base
   has_metadata 'descMetadata', type: EadMetadata
 
+  has_many :mets, :property=> :is_part_of
+
   has_attributes :title, datastream: 'descMetadata', multiple: false
   has_attributes :author, datastream: 'descMetadata', multiple: false
 
