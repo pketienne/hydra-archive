addcb9e53e67187c0cd181ba11b2d826bee807fc
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2014-05-16 00:19:54 -0400

    Added yet another metadata datastream.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,7 @@
 class Page < ActiveFedora::Base
   has_metadata 'pageMetadata', type: Datastreams::PageMetadata
   has_metadata 'wordMetadata', type: Datastreams::WordMetadata
+  has_metadata 'letterMetadata', type: Datastreams::LetterMetadata
 
   belongs_to :book, :property=> :is_part_of
 
@@ -9,5 +10,8 @@ class Page < ActiveFedora::Base
 
   has_attributes :title, datastream: 'wordMetadata', multiple: false
   has_attributes :author, datastream: 'wordMetadata', multiple: false
+
+  has_attributes :title, datastream: 'letterMetadata', multiple: false
+  has_attributes :author, datastream: 'letterMetadata', multiple: false
 
 end
--- content-model-test2.rb
+++ content-model-test2.rb
@@ -28,12 +28,19 @@ wm = Datastreams::WordMetadata.new
 wm.title = "blatherskite"
 wm.author = "I don't like unhappy families."
 
+# Setup new WordMetadata datastream
+lm = Datastreams::LetterMetadata.new
+lm.title = "blatherskite"
+lm.author = "I don't like unhappy families."
+
 # Setup new Page object
 p = Page.create()
 p.pageMetadata.title = "blathering"
 p.pageMetadata.author = "Happy families are all alike; every unhappy family is unhappy in its own way."
 p.wordMetadata.title = "blatherskite"
 p.wordMetadata.author = "I don't like unhappy families."
+p.letterMetadata.title = "foobar"
+p.letterMetadata.author = "I think I can, I think I can."
 
 # Associate Page with previous Book and save
 p.book = b
@@ -44,12 +51,16 @@ b.reload
 puts pm.to_xml
 puts p.pageMetadata
 puts p.wordMetadata
+puts p.letterMetadata
 puts p.title
 puts p.author
 puts p.pageMetadata.title
 puts p.pageMetadata.author
 puts p.wordMetadata.title
 puts p.wordMetadata.author
+puts p.letterMetadata.title
+puts p.letterMetadata.author
 puts p.to_solr
 puts p.pageMetadata.to_solr
 puts p.wordMetadata.to_solr
+puts p.letterMetadata.to_solr
