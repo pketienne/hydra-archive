6be2041d3e49093d32f8c95a86660a0d2d4a1788
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2014-05-23 15:41:00 -0400

    Further simplified files for testing purposes.

--- app/models/datastreams/ead_metadata.rb
+++ app/models/datastreams/ead_metadata.rb
@@ -1,13 +1,18 @@
-class EadMetadata < ActiveFedora::OmDatastream
+ class Datastreams::EadMetadata < ActiveFedora::OmDatastream
 
   set_terminology do |t|
-    t.root(path: "ead")
-    t.title(index_as: :stored_searchable)
-    t.author(index_as: :stored_searchable)
+    t.root(path: "fields")
+    t.eadheader {
+      t.eadid(index_as: :stored_searchable)
+      t.filedesc
+      t.profiledesc
+      t.revisiondesc
+    }
+    t.archdesc
   end
 
   def self.xml_template
-    Nokogiri::XML.parse("<ead/>")
+    Nokogiri::XML.parse("<fields/>")
   end
 
   def prefix
