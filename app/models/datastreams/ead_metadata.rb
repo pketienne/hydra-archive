350bf32f2d1bd57518e430b947e2b798ca245c15
Patrick K. Etienne <github@informatician.me>
2014-05-28 03:59:37 -0400

    Still attempting to create metadata datastream from file. Also added 'pry-history' as reference and renamed the current build script.

--- app/models/datastreams/ead_metadata.rb
+++ app/models/datastreams/ead_metadata.rb
@@ -1,14 +1,14 @@
- class Datastreams::EadMetadata < ActiveFedora::OmDatastream
+class Datastreams::EadMetadata < ActiveFedora::OmDatastream
 
   set_terminology do |t|
     t.root(path: "fields")
-    t.eadheader {
+    t.eadheader(index_as: :stored_searchable) {
       t.eadid(index_as: :stored_searchable)
       t.filedesc
       t.profiledesc
       t.revisiondesc
     }
-    t.archdesc
+    t.archdesc(index_as: :stored_searchable)
   end
 
   def self.xml_template
@@ -16,7 +16,8 @@
   end
 
   def prefix
-    # set a datastream prefix if you need to namespace terms that might occur in multiple data streams 
+    # set a datastream prefix if you need to namespace terms that
+    # might occur in multiple data streams 
     ""
   end
 
--- app/models/ead.rb
+++ app/models/ead.rb
@@ -1,6 +1,7 @@
 class Ead < ActiveFedora::Base
   has_metadata 'eadMetadata', type: Datastreams::EadMetadata
 
+  has_attributes :eadheader, datastream: 'eadMetadata', multiple: false
   has_attributes :archdesc, datastream: 'eadMetadata', multiple: false
 
 end
