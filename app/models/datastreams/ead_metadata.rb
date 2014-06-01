1b19059455dc78c088f5ea985fe30cc4dcd7b795
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2014-06-01 05:07:44 -0400

    Stripped previous demo files, added sample metadata and content datastream source files, setup content model.

--- app/models/datastreams/ead_metadata.rb
+++ app/models/datastreams/ead_metadata.rb
@@ -2,17 +2,10 @@ class EadMetadata < ActiveFedora::OmDatastream
 
   set_terminology do |t|
     t.root(path: "ead")
-    t.title
-    t.author
   end
 
   def self.xml_template
     Nokogiri::XML.parse("<ead/>")
-  end
-
-  def prefix
-    # set a datastream prefix if you need to namespace terms that might occur in multiple data streams 
-    ""
   end
 
 end
