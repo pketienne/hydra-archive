85bb2f5b291d34c8aa28dbe15ce8071581d3e3ab
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2014-06-04 20:15:00 -0400

    Need to get in better commit habits. Not yet commiting regularly at appropriate points.

--- app/models/datastreams/ead_metadata.rb
+++ app/models/datastreams/ead_metadata.rb
@@ -8,4 +8,15 @@ class EadMetadata < ActiveFedora::OmDatastream
     Nokogiri::XML.parse("<fields/>")
   end
 
+  def prefix
+    # set a datastream prefix if you need to namespace 
+    # terms that might occur in multiple data streams
+    
+    # DEPRECATION WARNING: In active-fedora 8 the solr fields created by EadMetadata will be prefixed
+    # with "ead_metadata__".  If you want to maintain the existing behavior, you must override 
+    # EadMetadata.#prefix to return an empty string.
+
+    ""
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,46 +3,46 @@ class Item < ActiveFedora::Base
 
   # Metadata for Browse Indexes
   has_attributes :title, datastream: 'itemMetadata', multiple: false
-  has_attributes :dateIssued , datastream: 'itemMetadata', multiple: false
+  has_attributes :date_issued , datastream: 'itemMetadata', multiple: false
 
   # Metadata for Query Results
   # repeat # has_attributes :title, datastream: 'itemMetadata', multiple: false
-  # repeat # has_attributes :date, datastream: 'itemMetadata', multiple: false
-  has_attributes :documentType, datastream: 'itemMetadata', multiple: false
-  has_attributes :uniqueId, datastream: 'itemMetadata', multiple: false
-  has_attributes :description, datastream: 'itemMetadata', multiple: false
+  # repeat # has_attributes :date_issued, datastream: 'itemMetadata', multiple: false
+  has_attributes :genre, datastream: 'itemMetadata', multiple: false
+  has_attributes :identifier, datastream: 'itemMetadata', multiple: false
+  has_attributes :abstract, datastream: 'itemMetadata', multiple: false
 
   # Metadata for Item Record
   ## Item Details
   # repeat # has_attributes :title, datastream: 'itemMetadata', multiple: false
-  # repeat # has_attributes :date, datastream: 'itemMetadata', multiple: false
-  # dynamic # has_attributes :creator, datastream: 'itemMetadata', multiple: false
-  # dynamic # has_attributes :interviewee, datastream: 'itemMetadata', multiple: false
-  # repeat # has_attributes :documentType, datastream: 'itemMetadata', multiple: false
-  # repeat # has_attributes :description, datastream: 'itemMetadata', multiple: false
+  # repeat # has_attributes :date_issued, datastream: 'itemMetadata', multiple: false
+  has_attributes :creator, datastream: 'itemMetadata', multiple: false
+  has_attributes :interviewee, datastream: 'itemMetadata', multiple: false
+  # repeat # has_attributes :genre, datastream: 'itemMetadata', multiple: false
+  # repeat # has_attributes :abstract, datastream: 'itemMetadata', multiple: false
   has_attributes :language, datastream: 'itemMetadata', multiple: false
   has_attributes :subject, datastream: 'itemMetadata', multiple: false
 
   ## Rights Information
-  has_attributes :copyrightHolder, datastream: 'itemMetadata', multiple: false
-  has_attributes :copyrightStatus, datastream: 'itemMetadata', multiple: false
-  has_attributes :rightsStatement, datastream: 'itemMetadata', multiple: false
+  has_attributes :access_condition_copyright_owner, datastream: 'itemMetadata', multiple: false
+  has_attributes :access_condition_status, datastream: 'itemMetadata', multiple: false
+  has_attributes :access_condition_statement, datastream: 'itemMetadata', multiple: false
 
   ## Physical Item Information
-  has_attributes :physicalSize, datastream: 'itemMetadata', multiple: false
+  has_attributes :note_source_dimensions, datastream: 'itemMetadata', multiple: false
   has_attributes :extent, datastream: 'itemMetadata', multiple: false
-  has_attributes :condition, datastream: 'itemMetadata', multiple: false
-  has_attributes :drawingType, datastream: 'itemMetadata', multiple: false
-  # repeat # has_attributes :uniqueId, datastream: 'itemMetadata', multiple: false
-  has_attributes :collection, datastream: 'itemMetadata', multiple: false
-  has_attributes :box, datastream: 'itemMetadata', multiple: false
-  has_attributes :folder, datastream: 'itemMetadata', multiple: false
+  has_attributes :note_condition, datastream: 'itemMetadata', multiple: false
+  # n/a # has_attributes :drawing_type, datastream: 'itemMetadata', multiple: false
+  # repeat # has_attributes :identifier, datastream: 'itemMetadata', multiple: false
+  has_attributes :related_item_identifier, datastream: 'itemMetadata', multiple: false
+  has_attributes :box_number, datastream: 'itemMetadata', multiple: false
+  has_attributes :folder_number, datastream: 'itemMetadata', multiple: false
 
   ## Available Files
-  has_attributes :type, datastream: 'itemMetadata', multiple: false
-  has_attributes :resolution, datastream: 'itemMetadata', multiple: false
-  has_attributes :fileName, datastream: 'itemMetadata', multiple: false
-  has_attributes :format, datastream: 'itemMetadata', multiple: false
+  # n/a # has_attributes :type, datastream: 'itemMetadata', multiple: false
+  # n/a # has_attributes :resolution, datastream: 'itemMetadata', multiple: false
+  has_attributes :id, datastream: 'itemMetadata', multiple: false
+  has_attributes :mimetype, datastream: 'itemMetadata', multiple: false
   has_attributes :size, datastream: 'itemMetadata', multiple: false
 
 end
