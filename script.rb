4a377e8b48c2454c1f8dd2d11f5cb36bca5e5b33
Patrick K. Etienne <github@informatician.me>
2014-06-06 18:03:45 -0400

    Setup an example for creating dynamically assigned content datastreams.

--- script.rb
+++ script.rb
@@ -1,7 +1,8 @@
 #! /usr/bin/ruby
 
-binding.pry
-
 book = Book.new
-book.bookContent.content = File.open('/home/petienne3/Projects/data-fulton-bag/ms004/ms004-001.pdf')
+book.bookContent.content = File.open('../../data-fulton-bag/ms004/ms004-001.pdf')
+ds = book.create_datastream(ActiveFedora::Datastream, "bookContent01")
+book.add_datastream(ds)
+book.datastreams['bookContent01'].content = File.open('../../data-fulton-bag/ms004/ms004-002.pdf')
 book.save
